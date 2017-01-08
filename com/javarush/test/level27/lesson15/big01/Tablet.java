4cb689fe363e0b2258dc6afde057e459196bebb9
Alexander <aav94@ya.ru>
2017-01-08 22:55:48 +0300

    lvl27 big 08 +

--- com/javarush/test/level27/lesson15/big01/Tablet.java
+++ com/javarush/test/level27/lesson15/big01/Tablet.java
@@ -1,9 +1,12 @@
 package com.javarush.test.level27.lesson15.big01;
 
+import com.javarush.test.level27.lesson15.big01.ad.AdvertisementManager;
 import com.javarush.test.level27.lesson15.big01.kitchen.Order;
 
 import java.io.IOException;
 import java.util.Observable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,13 +24,23 @@ public class Tablet extends Observable {
 
     public void createOrder()
     {
-        Order order;
+        final Order order;
         try
         {
                 order = new Order(this);
                 if (!order.isEmpty())
                 {
                     ConsoleHelper.writeMessage(order.toString());
+
+                    ExecutorService executor = Executors.newCachedThreadPool();
+                    executor.execute(new Thread()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            new AdvertisementManager(order.getTotalCookingTime()).processVideos();
+                        }
+                    });
                     setChanged();
                     notifyObservers(order);
                 }
--- com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
+++ com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
@@ -1,9 +1,22 @@
 package com.javarush.test.level27.lesson15.big01.ad;
 
+import com.javarush.test.level27.lesson15.big01.ConsoleHelper;
+
 /**
  * Created by Admin on 08.01.2017.
  */
 public class AdvertisementManager {
+
+    private int timeSeconds;
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public void processVideos()
+    {
+        ConsoleHelper.writeMessage("processVideos method calling");
+    }
 
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 
