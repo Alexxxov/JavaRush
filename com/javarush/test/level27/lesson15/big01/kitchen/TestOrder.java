cc4ec49bb00fab2ef334e1d8e563a52691dee36a
Alexander <aav94@ya.ru>
2017-01-21 17:05:38 +0300

    lvl27 big  done

--- com/javarush/test/level27/lesson15/big01/kitchen/TestOrder.java
+++ com/javarush/test/level27/lesson15/big01/kitchen/TestOrder.java
@@ -4,7 +4,7 @@ import com.javarush.test.level27.lesson15.big01.Tablet;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by Admin on 18.01.2017.
@@ -20,8 +20,7 @@ public class TestOrder extends Order {
     protected void initDishes()
     {
             this.dishes = new ArrayList<>();
-            Random random = new Random();
-            int rand = random.nextInt(Dish.values().length);
+            int rand = ThreadLocalRandom.current().nextInt(1, Dish.values().length);
             for (int i = 0; i < rand; i++) {
                 int num = (int) (Math.random() * Dish.values().length);
                 dishes.add(Dish.values()[num]);
