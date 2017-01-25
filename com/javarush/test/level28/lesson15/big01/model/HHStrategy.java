15755fbf66559998edd3d5660905538af24370ec
Alexander <aav94@yandex.ru>
2017-01-26 01:45:51 +0300

    level28.lesson15.big 06 +

--- com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.test.level28.lesson15.big01.model;
 
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +16,17 @@ public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
 
     @Override
-    public List<Vacancy> getVacancies(String searchString) {
+    public List<Vacancy> getVacancies(String searchString)
+    {
+
+        try
+        {
+            Document doc = Jsoup.connect("https://hh.ru").get();
+        }
+        catch (IOException e)
+        {
+
+        }
         return Collections.emptyList();
     }
 }
