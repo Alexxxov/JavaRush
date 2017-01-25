809cc401ff4db289cf3d9411d60d006e9a7afc5e
Alexander <aav94@yandex.ru>
2017-01-25 23:53:46 +0300

    level28.lesson15.big 02 +

--- com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,10 +1,17 @@
 package com.javarush.test.level28.lesson15.big01;
 
+import com.javarush.test.level28.lesson15.big01.model.Provider;
+import com.javarush.test.level28.lesson15.big01.model.Strategy;
+
 /**
  * Created by Admin on 24.01.2017.
  */
 public class Aggregator {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        Provider provider = new Provider(new Strategy() {
+        });
+        Controller controller = new Controller(provider);
+        System.out.println(controller);
     }
 }
--- com/javarush/test/level28/lesson15/big01/model/Strategy.java
+++ com/javarush/test/level28/lesson15/big01/model/Strategy.java
@@ -3,5 +3,7 @@ package com.javarush.test.level28.lesson15.big01.model;
 /**
  * Created by Admin on 24.01.2017.
  */
-public interface Strategy {
+public interface Strategy
+{
+
 }
